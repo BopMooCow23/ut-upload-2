75d8d93312bf2d02493130f5ebfe800407136a2b
BopMooCow23 <145719676+BopMooCow23@users.noreply.github.com>
2026-09-10 12:44:06 -0400

    even more bug fix :/

--- index.js
+++ index.js
@@ -959,28 +959,21 @@ if (/Android|iPhone|iPod/i.test(navigator.userAgent)) {
 
 document.addEventListener("visibilitychange", () => {
   if (document.visibilityState !== "visible") {
-   
     try {
       pause();
     } catch (error) {
       console.error("Game pause failed:", error);
     }
   } else {
-    
     try {
-      if (typeof GM_is_multiplayer === "function") {
-        if (GM_is_multiplayer()) {
-          resume();
-        }
-      } else {
-        
-        resume();
-      }
+      resume();
     } catch (error) {
       console.error("Game resume failed:", error);
     }
   }
 });
+
+
 
 window.addEventListener("load", (event) => {
   if ((!window.oprt || !window.oprt.enterFullscreen) && (!window.chrome || !window.chrome.runtime || !window.chrome.runtime.sendMessage)) {
